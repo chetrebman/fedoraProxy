28ce611e39791887bf95faa8270e95126d29301d
chetrebman <chet2003@cybox.com>
2012-11-19 19:00:25 -0700

    fix header

--- src/edu/du/penrose/systems/fedoraProxy/web/bus/IngestController.java
+++ src/edu/du/penrose/systems/fedoraProxy/web/bus/IngestController.java
@@ -4,18 +4,18 @@
  * 
  * This file is part of FedoraProxy.
  * 
- * FedoraApp is free software: you can redistribute it and/or modify
+ * FedoraProxy is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
- * FedoraApp is distributed in the hope that it will be useful,
+ * FedoraProxy is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public License
- * along with FedoraApp.  If not, see <http://www.gnu.org/licenses/>.
+ * along with FedoraProxy.  If not, see <http://www.gnu.org/licenses/>.
 */
 package edu.du.penrose.systems.fedoraProxy.web.bus;
 
